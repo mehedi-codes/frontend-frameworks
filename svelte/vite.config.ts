b3c2cde1cc7ce0341f07160821bcc0c8caf893e7
Mehedi Hasan <mehedi@etlimited.net>
2026-07-26 21:55:49 +0600

    svelte initial setup

---
+++ svelte/vite.config.ts
@@ -0,0 +1,12 @@
+import path from "node:path";
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  server: {
+    port: 5001
+  }
+})
